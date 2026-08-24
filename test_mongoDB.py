3ea2568d2d26dca8935a1daeb8adc9bdc82f5103
Nithin <23mm01024@iitbbs.ac.in>
2026-08-24 23:48:32 +0530

    ETL pipeliline completed

---
+++ test_mongoDB.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+uri = "mongodb+srv://23mm01024_db_user:tZepbeSCM4mhvzxR@cluster0.ujvp24u.mongodb.net/?appName=Cluster0"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
